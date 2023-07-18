20d8f63e0b1e74631b72a6ba87c1924ce88a3368
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-07-18 13:57:38 +0900

    User configuration

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react' // we are using hooks -> need client side rendering
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
-import { set } from 'mongoose';
+
 
 const Nav = () => {
 
@@ -19,9 +19,8 @@ const Nav = () => {
             setProviders(response)
         }
 
-        setProviders();
+        setUpProviders();
     }, [])
-
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'> 
@@ -33,9 +32,7 @@ const Nav = () => {
             />
             <p className='logo_text'>Promptopia</p>
         </Link>
-
         
-
         {/* Desktop Navigation */}
         <div className='sm:flex hidden'>
             {session?.user ? (
@@ -48,7 +45,7 @@ const Nav = () => {
                     </button>
                     <Link href="/profile">
                         <Image 
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
@@ -78,7 +75,7 @@ const Nav = () => {
             {session?.user ? (
                 <div className='flex'>
                     <Image 
-                        src="/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 import { Schema, model, models } from 'mongoose'
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     email: {
         type: String,
         unique: [true, 'Email already exist'],
@@ -8,14 +8,12 @@ const userSchema = new Schema({
     },
     username: {
         type: String,
-        required: [true, 'Username is required!'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
     image: {
         type: String,
     }
 })
 
-const User = models.User || model("User", userSchema)
+const User = models.User || model("User", UserSchema)
 // this code is called every time it is requested so it searches form models.User first
 export default User;
