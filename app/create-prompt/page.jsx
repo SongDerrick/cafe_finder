10bf69abe5d09485b98451197c87785932095a02
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-07-18 16:17:46 +0900

    Prompt Shown

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from 'react'
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form'
+
+const CreatePrompt = () => {
+    const router = useRouter();
+    const { data: session } = useSession();
+    const [submitting, setIsSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+    const createPrompt = async (e) => {
+        e.preventDefault(); // prevent browser default actions
+        setIsSubmitting(true);
+
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+            if(response.ok){
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsSubmitting(false);
+        }
+
+
+    }
+  return (
+    <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+    />
+  )
+}
+
+export default CreatePrompt
